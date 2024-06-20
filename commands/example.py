90f0676a7654e7f917c7e184ea6b60663a9dbcc6
Thomas Young <dosmaen@gmail.com>
2024-06-20 06:55:40 -0700

    Updated the quotes to make it look a bit better using embeds.

--- commands/example.py
+++
@@ -1,8 +0,0 @@
-from discord.ext import commands
-
-@commands.command()
-async def test(ctx):
-    await ctx.send("This is a test command. If you can see this, then the bot is working!")
-
-async def setup(bot):
-    bot.add_command(test)
--- main.py
+++ main.py
@@ -14,14 +14,17 @@ intents.message_content = True;
 
 client = commands.Bot(command_prefix = "!", intents=intents)
 
+
 @client.event
 async def on_ready():
     print(str(client.user) + " is now online!")
+    
 
 async def load_extensions():
     for filename in os.listdir("./commands"):
         if filename.endswith(".py"):
             await client.load_extension(f"commands.{filename[:-3]}")
+    
 
 async def main():
     async with client:
