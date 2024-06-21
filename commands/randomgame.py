6f034ce04db4dc1f8b5b5fbe2bc0b054b8b89c3c
Thomas Young <dosmaen@gmail.com>
2024-06-20 21:12:31 -0700

    Made the randomgame embed look prettier with image

--- commands/randomgame.py
+++ commands/randomgame.py
@@ -6,27 +6,35 @@ import discord
 from data import game
 MAX_PAGE = 155
 BASE_URL = 'https://itch.io/games/free.xml?page='
-games = [game.Game("test", "placeholder")]
+games = []
+
 @commands.command()
 async def randomgame(ctx):
     names = []
     links = []
+    imgs = []
     page = BASE_URL + str(random.randint(0,MAX_PAGE+1))
     list_from_itch = requests.get(page)
     xml_document = list_from_itch.content
 
     root = ET.fromstring(xml_document)
-
+    
+    count = len(root.findall('channel/item/title'))
+    
     for gameTitle in root.findall('channel/item/title'):
         names.append(gameTitle.text)
+    
     for gameLink in root.findall('channel/item/link'):
         links.append(gameLink.text)
-    for title, link in zip(names, links):
-        games.append(game.Game(title, link))
-    print(games[1].name)
+    
+    for gameImage in root.findall('channel/item/imageurl'):
+        imgs.append(gameImage.text)
+    
+    for title, link, img in zip(names, links, imgs):
+        games.append(game.Game(title, link, img))
     rg = random.randint(1, len(names))
     embedVar = discord.Embed(title=games[rg].name, description=games[rg].link, color=discord.Color.blue())
-    
+    embedVar.set_image(url=games[rg].img)
     await ctx.send("", embed=embedVar, ephemeral=True)
 
         
--- data/game.py
+++ data/game.py
@@ -1,4 +1,5 @@
 class Game:
-   def __init__(self, name, link):
+   def __init__(self, name, link,img):
     self.name = name
     self.link = link
+    self.img = img
